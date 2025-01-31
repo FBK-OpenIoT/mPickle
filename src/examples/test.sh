c3a72d9f720d20b1f45eb419be1a9db595ba73a8
Mattia Antonini <m.antonini@fbk.eu>
2025-01-31 15:54:04 +0100

    Update messages

--- src/examples/test.sh
+++ src/examples/test.sh
@@ -10,7 +10,7 @@ run_test() {
     OUT1=$($CPYTHON pickle_tests.py --example_name "$1" --dump)
     STATUS1=$?
     if [ "$STATUS1" -ne 0 ]; then
-        echo -e "\e[0;31mFAIL: CPython dump failed for $1\e[0m"
+        echo -e "\e[0;31mFAIL: CPython Pickle dump failed for $1\e[0m"
         return
     fi
 
@@ -19,18 +19,18 @@ run_test() {
     OUT2_LAST_LINE=$(echo "$OUT2" | tail -n 1)  # Extract last line separately
 
     if [ "$STATUS2" -ne 0 ]; then
-        echo -e "\e[0;31mFAIL: MicroPython load failed for $1\e[0m"
+        echo -e "\e[0;31mFAIL: MicroPython mPickle load failed for $1\e[0m"
         return
     fi
 
     rm -f dump.pkl
-    echo "Test Dump: CPython -> Load: MicroPython): $OUT2_LAST_LINE"
+    echo "Test Dump: CPython (pickle) -> Load: MicroPython (mpickle): $OUT2_LAST_LINE"
 
     # MicroPython -> CPython
     OUT3=$($MPYTHON pickle_tests.py --example_name "$1" --dump)
     STATUS3=$?
     if [ "$STATUS3" -ne 0 ]; then
-        echo -e "\e[0;31mFAIL: MicroPython dump failed for $1\e[0m"
+        echo -e "\e[0;31mFAIL: MicroPython mPickle dump failed for $1\e[0m"
         return
     fi
 
@@ -39,19 +39,19 @@ run_test() {
     OUT4_LAST_LINE=$(echo "$OUT4" | tail -n 1)
 
     if [ "$STATUS4" -ne 0 ]; then
-        echo -e "\e[0;31mFAIL: CPython load failed for $1\e[0m"
+        echo -e "\e[0;31mFAIL: CPython Pickle load failed for $1\e[0m"
         return
     fi
 
     rm -f dump.pkl
-    echo "Test Dump: MicroPython -> Load: CPython): $OUT4_LAST_LINE"
+    echo "Test Dump: MicroPython (mpickle) -> Load: CPython (pickle): $OUT4_LAST_LINE"
 
     # MicroPython -> MicroPython
 
     OUT5=$($MPYTHON pickle_tests.py --example_name "$1" --dump)
     STATUS5=$?
     if [ "$STATUS5" -ne 0 ]; then
-        echo -e "\e[0;31mFAIL: MicroPython dump failed for $1\e[0m"
+        echo -e "\e[0;31mFAIL: MicroPython mPickle dump failed for $1\e[0m"
         return
     fi
 
@@ -60,15 +60,15 @@ run_test() {
     OUT6_LAST_LINE=$(echo "$OUT6" | tail -n 1)
 
     if [ "$STATUS6" -ne 0 ]; then
-        echo -e "\e[0;31mFAIL: MicroPython load failed for $1\e[0m"
+        echo -e "\e[0;31mFAIL: MicroPython mPickle load failed for $1\e[0m"
         echo $OUT6
         return
     fi
 
     rm -f dump.pkl
-    echo "Test Dump: MicroPython -> Load: MicroPython): $OUT6_LAST_LINE"
+    echo "Test Dump: MicroPython (mpickle) -> Load: MicroPython (mpickle): $OUT6_LAST_LINE"
 
-    echo -e "\e[0;32mTest passed for $1\e[0m"
+    echo -e "\e[0;32mTest passed for $1\e[0m\n"
 }
 
 run_test "builtins-data-types"
