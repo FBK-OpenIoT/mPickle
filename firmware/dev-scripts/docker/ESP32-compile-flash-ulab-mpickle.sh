01f0c7fa8b0b35b124414b31bc17107ca40efa78
Mattia Antonini <m.antonini@fbk.eu>
2025-12-15 12:26:59 +0100

    Fix ESP32 building script and added support for variants

--- firmware/dev-scripts/docker/ESP32-compile-flash-ulab-mpickle.sh
+++ firmware/dev-scripts/docker/ESP32-compile-flash-ulab-mpickle.sh
@@ -32,39 +32,58 @@
 . ./common.sh
 
 BOARD=${1:-ESP32_GENERIC}
+if [ -n "$2" ]; then
+   BOARD_VARIANT="-${2}"
+fi
 # build with 4 dimensions
 MATRIX_DIMENSIONS=4
+PORT_DIR=esp32
+BOARD_DIR=$PORT_DIR/boards/$BOARD
 
-cd /mpy-compile/esp-idf
-. ./export.sh
-cd /mpy-compile/
+# Load ESP-IDF environment
+. $IDF_PATH/export.sh
 
-cd /mpy-compile/micropython/ports/esp32
-make submodules
-cd /mpy-compile/
+# Configure build with ulab and mPickle frozen modules
+USER_C_MODULES=/mpy-compile/ulab/code/micropython.cmake
+MPICKLE_FROZEN_MANIFEST=/mpy-compile/modules/mPickle/manifest.py
+FROZEN_MANIFEST=/mpy-compile/manifest.py
 
-# Create the Makefile for the compilation
-cat > "/mpy-compile/micropython/ports/esp32/makefile" <<EOL
-BOARD = $BOARD
-USER_C_MODULES = /mpy-compile/ulab/code/micropython.cmake
-FROZEN_MANIFEST=/mpy-compile/modules/mPickle/manifest.py
+echo "include(\"$MPICKLE_FROZEN_MANIFEST\")" >> $FROZEN_MANIFEST
 
-CFLAGS += -DULAB_HAS_DTYPE_OBJECT=1
-CFLAGS += -DULAB_MAX_DIMS=4
-
-include Makefile
-EOL
-
-# Confirm the Makefile was created
-if [ -f "/mpy-compile/micropython/ports/esp32/makefile" ]; then
-    echo "Makefile successfully created at /mpy-compile/micropython/ports/esp32/makefile"
-else
-    echo "Failed to create Makefile!"
-    exit 1
+if [ -f /mpy-compile/micropython/ports/$BOARD_DIR/manifest.py ]; then
+    echo "include(\"/mpy-compile/micropython/ports/$BOARD_DIR/manifest.py\")" >> $FROZEN_MANIFEST
+elif [ -f /mpy-compile/micropython/ports/$PORT_DIR/boards/manifest.py ]; then
+    echo "include(\"/mpy-compile/micropython/ports/$PORT_DIR/boards/manifest.py\")" >> $FROZEN_MANIFEST
 fi
 
+# Extra compilation flags for ulab
+EXTRA_FLAGS="-DULAB_HAS_DTYPE_OBJECT=1 -DULAB_MAX_DIMS=${MATRIX_DIMENSIONS}"
+
+# Build MicroPython firmware
 cd /mpy-compile/micropython/ports/esp32
-make
-echo "Flashing..."
-make deploy
-cd /mpy-compile/
+# IDF_FLAGS="-D MICROPY_BOARD=$BOARD -D MICROPY_BOARD_DIR=/mpy-compile/micropython/ports/esp32/boards/$BOARD -D USER_C_MODULES=$USER_C_MODULES  -DCMAKE_C_FLAGS="$EXTRA_FLAGS" -DCMAKE_CXX_FLAGS="$EXTRA_FLAGS""
+IDF_FLAGS="-D MICROPY_BOARD=$BOARD -D MICROPY_BOARD_DIR=/mpy-compile/micropython/ports/esp32/boards/$BOARD -D USER_C_MODULES=$USER_C_MODULES -D MICROPY_FROZEN_MANIFEST=$FROZEN_MANIFEST -DCMAKE_C_FLAGS="$EXTRA_FLAGS" -DCMAKE_CXX_FLAGS="$EXTRA_FLAGS""
+[ -n "$BOARD_VARIANT" ] && IDF_FLAGS="$IDF_FLAGS -DMICROPY_BOARD_VARIANT=${BOARD_VARIANT#'-'}"
+
+make -j $(nproc) BOARD=$BOARD submodules
+BUILD=/mpy-compile/micropython/ports/esp32/build-$BOARD$BOARD_VARIANT
+mkdir -p $BUILD
+
+cmake -S . -B $BUILD $IDF_FLAGS  -GNinja
+ninja -j $(nproc) -C $BUILD
+python3 makeimg.py \
+   $BUILD/sdkconfig \
+   $BUILD/bootloader/bootloader.bin \
+   $BUILD/partition_table/partition-table.bin \
+   $BUILD/micropython.bin \
+   $BUILD/firmware.bin \
+   $BUILD/micropython.uf2
+
+# Flash firmware to the device
+cd $BUILD
+esptool.py erase_flash
+esptool.py write_flash $(cat flash_args)
+
+echo "Flashing completed!"
+# End of script
+ 
